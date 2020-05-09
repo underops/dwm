ff2709f1964d5deb59720bfb87577fd494aab207
kdecarli <kauandecarli@gmail.com>
2020-05-08 21:32:30 -0300

    slightly change bar active tag indicator

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int bar_anim			= 1;		/* 0 means no animation */
 static const char *fonts[]          = { "Misc terminusmodx:size=9" };
-static const char dmenufont[]       = "Misc terminusmodx";
+static const char dmenufont[]       = "Misc terminusmodx:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
