a0bb49318e0e14f914354bead2b781eac18713de
eksplosion <kauandecarli@gmail.com>
2021-02-08 15:22:37 -0300

    Remove 'alpha' patch

--- config.h
+++ config.h
@@ -16,17 +16,10 @@ static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha  = 0xb2;
-static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray0, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan },
-};
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
--- dwm.c
+++ dwm.c
@@ -60,8 +60,6 @@
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
 
-#define OPAQUE                  0xffU
-
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
 enum { SchemeNorm, SchemeSel }; /* color schemes */
@@ -244,7 +242,6 @@ static Monitor *wintomon(Window w);
 static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
-static void xinitvisual();
 static void zoom(const Arg *arg);
 
 static pid_t getparentprocess(pid_t p);
@@ -287,11 +284,6 @@ static Display *dpy;
 static Drw *drw;
 static Monitor *mons, *selmon;
 static Window root, wmcheckwin;
-
-static int useargb = 0;
-static Visual *visual;
-static int depth;
-static Colormap cmap;
 
 static xcb_connection_t *xcon;
 
@@ -938,13 +930,13 @@ drawstatusbar(Monitor *m, int bh, char* stext) {
                     char buf[8];
                     memcpy(buf, (char*)text+i+1, 7);
                     buf[7] = '\0';
-                    drw_clr_create(drw, &drw->scheme[ColFg], buf, alphas[ColFg][0]);
+                    drw_clr_create(drw, &drw->scheme[ColFg], buf);
                     i += 7;
                 } else if (text[i] == 'b') {
                     char buf[8];
                     memcpy(buf, (char*)text+i+1, 7);
                     buf[7] = '\0';
-                    drw_clr_create(drw, &drw->scheme[ColBg], buf, alphas[ColBg][1]);
+                    drw_clr_create(drw, &drw->scheme[ColBg], buf);
                     i += 7;
                 } else if (text[i] == 'd') {
                     drw->scheme[ColFg] = scheme[SchemeNorm][ColFg];
@@ -2002,8 +1994,7 @@ setup(void)
     sw = DisplayWidth(dpy, screen);
     sh = DisplayHeight(dpy, screen);
     root = RootWindow(dpy, screen);
-    xinitvisual();
-    drw = drw_create(dpy, screen, root, sw, sh, visual, depth, cmap);
+    drw = drw_create(dpy, screen, root, sw, sh);
     if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
         die("no fonts could be loaded.");
     lrpad = drw->fonts->h;
@@ -2030,9 +2021,9 @@ setup(void)
     cursor[CurMove] = drw_cur_create(drw, XC_fleur);
     /* init appearance */
     scheme = ecalloc(LENGTH(colors) + 1, sizeof(Clr *));
-    scheme[LENGTH(colors)] = drw_scm_create(drw, colors[0], alphas[0], 3);
+    scheme[LENGTH(colors)] = drw_scm_create(drw, colors[0], 3);
     for (i = 0; i < LENGTH(colors); i++)
-        scheme[i] = drw_scm_create(drw, colors[i], alphas[i], 3);
+        scheme[i] = drw_scm_create(drw, colors[i], 3);
     /* init bars */
     updatebars();
     updatestatus();
@@ -2296,18 +2287,16 @@ updatebars(void)
     Monitor *m;
     XSetWindowAttributes wa = {
         .override_redirect = True,
-        .background_pixel = 0,
-        .border_pixel = 0,
-        .colormap = cmap,
+        .background_pixmap = ParentRelative,
         .event_mask = ButtonPressMask|ExposureMask
     };
     XClassHint ch = {"dwm", "dwm"};
     for (m = mons; m; m = m->next) {
         if (m->barwin)
             continue;
-        m->barwin = XCreateWindow(dpy, root, m->wx, m->by, m->ww, bh, 0, depth,
-                                  InputOutput, visual,
-                                  CWOverrideRedirect|CWBackPixel|CWBorderPixel|CWColormap|CWEventMask, &wa);
+        m->barwin = XCreateWindow(dpy, root, m->wx, m->by, m->ww, bh, 0, DefaultDepth(dpy, screen),
+                                  CopyFromParent, DefaultVisual(dpy, screen),
+                                  CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
         XDefineCursor(dpy, m->barwin, cursor[CurNormal]->cursor);
         XMapRaised(dpy, m->barwin);
         XSetClassHint(dpy, m->barwin, &ch);
@@ -2706,43 +2695,6 @@ xerrorstart(Display *dpy, XErrorEvent *ee)
 {
     die("dwm: another window manager is already running");
     return -1;
-}
-
-void
-xinitvisual()
-{
-    XVisualInfo *infos;
-    XRenderPictFormat *fmt;
-    int nitems;
-    int i;
-
-    XVisualInfo tpl = {
-        .screen = screen,
-        .depth = 32,
-        .class = TrueColor
-    };
-    long masks = VisualScreenMask | VisualDepthMask | VisualClassMask;
-
-    infos = XGetVisualInfo(dpy, masks, &tpl, &nitems);
-    visual = NULL;
-    for(i = 0; i < nitems; i ++) {
-        fmt = XRenderFindVisualFormat(dpy, infos[i].visual);
-        if (fmt->type == PictTypeDirect && fmt->direct.alphaMask) {
-            visual = infos[i].visual;
-            depth = infos[i].depth;
-            cmap = XCreateColormap(dpy, root, visual, AllocNone);
-            useargb = 1;
-            break;
-        }
-    }
-
-    XFree(infos);
-
-    if (! visual) {
-        visual = DefaultVisual(dpy, screen);
-        depth = DefaultDepth(dpy, screen);
-        cmap = DefaultColormap(dpy, screen);
-    }
 }
 
 void
